a8860ae427e97c9222a152a8f43158aa6a513078
Chirag-1011 <devmurarichirag007@gmail.com>
2024-07-08 17:58:27 +0530

    DAY-5

---
+++ DAY 5/q-7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    for (int i='A'; i<='E'; i++)
+    {
+        for (int j=i; j>='A'; j--)
+        {
+            printf(" %c",j);
+        }
+        printf("\n");
+        
+    }
+    
+}
